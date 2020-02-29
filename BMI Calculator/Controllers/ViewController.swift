f9e498949c3a7c4c99ff8a0b77ff77aeb9774fb3
Tam Phan <tam.phan@mac.com>
2020-02-28 23:16:18 -0800

    Sliders that Tam worked on

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+	@IBOutlet weak var heightSliderLabel: UILabel!
+	@IBOutlet weak var weightSliderLabel: UILabel!
+	
+	var height: Double = 1.5
+	var weight: Int = 100
+	
+	override func viewDidLoad() {
+	super.viewDidLoad()
+	
+	}
 
-
+	@IBAction func heightSliderChanged(_ sender: UISlider) {
+		// Get height value from slider
+		height = Double(sender.value)
+		// Round up "height: Double" to only 2 decimal spaces
+		let heightConverted = String(format: "%.2f", height)
+		// Display height to slider label
+		heightSliderLabel.text = "\(heightConverted)m"
+	}
+	
+	@IBAction func weightSliderChanged(_ sender: UISlider) {
+		weight = Int(sender.value)
+		weightSliderLabel.text = "\(weight)Kg"
+	}
+	
 }
 
 //first comment
